0d617d7f0f41e1cd75ea4453bc7f1fe7c4399c5b
Christine Hwang <cjhwang06@gmail.com>
2014-08-19 22:38:45 -0700

    Create first_reverse_alt01.py

---
+++ first_reverse_alt01.py
@@ -0,0 +1,9 @@
+def FirstReverse(str_): 
+
+  # code goes here
+  return ''.join(reversed(str_))
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstReverse(raw_input())     
